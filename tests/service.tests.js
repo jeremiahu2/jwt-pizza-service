5d3d3382771246823d5a804f673309cf21d60d70
Jeremiah Barton <boateng@byu.edu>
2026-01-30 23:11:47 -0700

    added describe test

--- tests/service.tests.js
+++ tests/service.tests.js
@@ -1,3 +1,11 @@
 const request = require('supertest');
 const app = require('../src/service');
 
+describe('JWT Pizza Service', () => {
+  test('POST /login should return JWT token', async () => {
+    const res = await request(app).post('/login');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('authorization');
+    expect(typeof res.body.authorization).toBe('string');
+  });
+});
