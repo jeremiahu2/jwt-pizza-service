efab68c27a32a0c37be8b892eb27bc7fb97c142e
Jeremiah Barton <boateng@byu.edu>
2026-01-31 17:40:39 -0700

    updated required path

--- tests/service.test.js
+++ tests/service.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('../src/index');
 const db = require('../src/database/dbModel');
 const service = require('../src/service/service');
 
