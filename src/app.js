7e52075cb22a3a97b2b5cc9ca38c4e4ac122d82d
Jeremiah Barton <boateng@byu.edu>
2026-01-31 20:37:23 -0700

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const { authRouter, setAuthUser } = require('./routes/authRouter');
 const franchiseRouter = require('./routes/franchiseRouter');
 const orderRouter = require('./routes/orderRouter');
 const userRouter = require('./routes/userRouter');
+const version = require('./version.json');
 const app = express();
-app.use(express.json());
-app.use(setAuthUser);
 
+app.use(express.json());
+app.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(setAuthUser);
 app.use('/api/auth', authRouter);
 app.use('/api/franchise', franchiseRouter);
 app.use('/api/order', orderRouter);
 app.use('/api/user', userRouter);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'welcome to JWT Pizza', version: version.version });
+});
+app.use((req, res) => {
+  res.status(404).json({ message: 'unknown endpoint' });
+});
+app.use((err, req, res, next) => {
+  res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
+});
 
 module.exports = app;
